c7bd620996eec6b8bce79c62d23f8202f157f8e1
gerardogarnica <gerardo.garnica@gmail.com>
2025-03-07 15:17:04 -0500

    Add currency and money value objects

---
+++ src/Aurora.Coinly.Domain/Shared/Currency.cs
@@ -0,0 +1,23 @@
+﻿namespace Aurora.Coinly.Domain.Shared;
+
+public record Currency
+{
+    internal static readonly Currency Default = new("");
+    public static readonly Currency Usd = new("USD");
+    public static readonly Currency Eur = new("EUR");
+
+    public string Code { get; init; }
+
+    public static readonly IReadOnlyCollection<Currency> All =
+    [
+        Usd,
+        Eur
+    ];
+
+    public Currency(string code) => Code = code;
+
+    public static Currency FromCode(string code)
+    {
+        return All.FirstOrDefault(c => c.Code == code) ?? Default;
+    }
+}
---
+++ src/Aurora.Coinly.Domain/Shared/Money.cs
@@ -0,0 +1,29 @@
+﻿namespace Aurora.Coinly.Domain.Shared;
+
+public record Money(decimal Amount, Currency Currency)
+{
+    public static Money Zero() => Zero(Currency.Default);
+    public static Money Zero(Currency currency) => new(0, currency);
+
+    public bool IsZero() => this == Zero(Currency);
+
+    public static Money operator +(Money first, Money second)
+    {
+        if (first.Currency != second.Currency)
+        {
+            throw new InvalidOperationException("Currencies have to be equal");
+        }
+
+        return new Money(first.Amount + second.Amount, first.Currency);
+    }
+
+    public static Money operator -(Money first, Money second)
+    {
+        if (first.Currency != second.Currency)
+        {
+            throw new InvalidOperationException("Currencies have to be equal");
+        }
+
+        return new Money(first.Amount - second.Amount, first.Currency);
+    }
+}
