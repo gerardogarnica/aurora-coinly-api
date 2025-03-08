614ad7e3486547548f2536cf997b9b9d9c05f8a2
gerardogarnica <gerardo.garnica@gmail.com>
2025-03-08 16:06:23 -0500

    Add payment method domain entities

---
+++ src/Aurora.Coinly.Domain/Methods/IPaymentMethodRepository.cs
@@ -0,0 +1,7 @@
+﻿namespace Aurora.Coinly.Domain.Methods;
+
+public interface IPaymentMethodRepository : IRepository<PaymentMethod>
+{
+    Task<PaymentMethod?> GetByIdAsync(Guid id);
+    Task<IEnumerable<PaymentMethod>> GetListAsync(bool showDeleted);
+}
---
+++ src/Aurora.Coinly.Domain/Methods/PaymentMethodErrors.cs
@@ -0,0 +1,12 @@
+﻿namespace Aurora.Coinly.Domain.Methods;
+
+public static class PaymentMethodErrors
+{
+    public static readonly BaseError NotFound = new(
+        "PaymentMethod.Found",
+        "The payment method with the specified identifier was not found");
+
+    public static readonly BaseError IsDeleted = new(
+        "PaymentMethod.IsDeleted",
+        "The payment method is deleted");
+}
