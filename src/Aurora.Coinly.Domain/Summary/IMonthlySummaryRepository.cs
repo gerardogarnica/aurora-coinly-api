af07f8924164d30da90b6d4b584c05f3d01c058b
gerardogarnica <gerardo.garnica@gmail.com>
2025-03-09 13:44:33 -0500

    Add summary domain entities

---
+++ src/Aurora.Coinly.Domain/Summary/IMonthlySummaryRepository.cs
@@ -0,0 +1,7 @@
+﻿namespace Aurora.Coinly.Domain.Summary;
+
+public interface IMonthlySummaryRepository : IRepository<MonthlySummary>
+{
+    Task<MonthlySummary?> GetSummaryAsync(int year, int month);
+    Task<IEnumerable<MonthlySummary>> GetListAsync(int year);
+}
---
+++ src/Aurora.Coinly.Domain/Summary/SummaryErrors.cs
@@ -0,0 +1,16 @@
+﻿namespace Aurora.Coinly.Domain.Summary;
+
+public static class SummaryErrors
+{
+    public static readonly BaseError TransactionAlreadyIsPaid = new(
+        "Summary.TransactionAlreadyIsPaid",
+        "The transaction is already paid");
+
+    public static readonly BaseError TransactionNotInPeriod = new(
+        "Summary.TransactionNotInPeriod",
+        "The transaction is not in the summary period");
+
+    public static readonly BaseError TransactionNotPaid = new(
+        "Summary.TransactionNotPaid",
+        "The transaction is not paid");
+}
