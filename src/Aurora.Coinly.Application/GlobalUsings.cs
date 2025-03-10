f9346b5f07a27aa5eaf7d4915be5aac62dd5f5e6
gerardogarnica <gerardo.garnica@gmail.com>
2025-03-10 09:53:14 -0500

    Add domain event handler interface

--- src/Aurora.Coinly.Application/GlobalUsings.cs
+++ src/Aurora.Coinly.Application/GlobalUsings.cs
@@ -1,4 +1,5 @@
 ﻿global using Aurora.Coinly.Application.Abstractions.Messaging;
+global using Aurora.Coinly.Application.Abstractions.Time;
 global using Aurora.Coinly.Domain.Abstractions;
 global using Aurora.Coinly.Domain.Shared;
 global using FluentValidation;
