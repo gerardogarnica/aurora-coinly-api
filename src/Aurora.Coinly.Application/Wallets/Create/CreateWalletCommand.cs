d6a2b4bf6212d4de2bdd3e2926c648aafeb03b5a
gerardogarnica <gerardo.garnica@gmail.com>
2025-03-10 11:59:21 -0500

    Add create wallet command

---
+++ src/Aurora.Coinly.Application/Wallets/Create/CreateWalletCommand.cs
@@ -0,0 +1,10 @@
+﻿using Aurora.Coinly.Domain.Wallets;
+
+namespace Aurora.Coinly.Application.Wallets.Create;
+
+public sealed record CreateWalletCommand(
+    string Name,
+    string CurrencyCode,
+    decimal Amount,
+    WalletType Type,
+    string? Notes) : IRequest<Guid>;
---
+++ src/Aurora.Coinly.Application/Wallets/Create/CreateWalletCommandValidator.cs
@@ -0,0 +1,27 @@
+﻿namespace Aurora.Coinly.Application.Wallets.Create;
+
+internal sealed class CreateWalletCommandValidator : AbstractValidator<CreateWalletCommand>
+{
+    public CreateWalletCommandValidator()
+    {
+        RuleFor(x => x.Name)
+            .NotEmpty()
+            .MinimumLength(3)
+            .MaximumLength(100);
+
+        RuleFor(x => x.CurrencyCode)
+            .NotEmpty()
+            .Must(BeAValidCurrencyCode).WithMessage("'{PropertyName}' is not a valid currency code.");
+
+        RuleFor(x => x.Amount)
+            .GreaterThanOrEqualTo(0)
+            .PrecisionScale(2, 9, true);
+
+        RuleFor(x => x.Notes).MaximumLength(1000);
+    }
+
+    private static bool BeAValidCurrencyCode(string currencyCode)
+    {
+        return Currency.All.Contains(Currency.FromCode(currencyCode));
+    }
+}
