04bf10a50408e4deeb9bac6332b3ebb3a2eafce5
gerardogarnica <gerardo.garnica@gmail.com>
2025-03-10 15:19:28 -0500

    Add add transaction to wallet command

---
+++ src/Aurora.Coinly.Application/Wallets/AddTransaction/AddTransactionToWalletCommand.cs
@@ -0,0 +1,5 @@
+﻿namespace Aurora.Coinly.Application.Wallets.AddTransaction;
+
+public sealed record AddTransactionToWalletCommand(
+    Guid WalletId,
+    Guid TransactionId) : ICommand;
---
+++ src/Aurora.Coinly.Application/Wallets/AddTransaction/AddTransactionToWalletCommandHandler.cs
@@ -0,0 +1,56 @@
+﻿using Aurora.Coinly.Domain.Transactions;
+using Aurora.Coinly.Domain.Wallets;
+
+namespace Aurora.Coinly.Application.Wallets.AddTransaction;
+
+internal sealed class AddTransactionToWalletCommandHandler(
+    IWalletRepository walletRepository,
+    ITransactionRepository transactionRepository,
+    IDateTimeService dateTimeService) : ICommandHandler<AddTransactionToWalletCommand>
+{
+    public async Task<Result> Handle(
+        AddTransactionToWalletCommand request,
+        CancellationToken cancellationToken)
+    {
+        // Get wallet
+        var wallet = await walletRepository.GetByIdAsync(request.WalletId);
+        if (wallet is null)
+        {
+            return Result.Fail(WalletErrors.NotFound);
+        }
+
+        // Get transaction
+        var transaction = await transactionRepository.GetByIdAsync(request.TransactionId);
+        if (transaction is null)
+        {
+            return Result.Fail(TransactionErrors.NotFound);
+        }
+
+        if (!transaction.IsPaid)
+        {
+            return Result.Fail(TransactionErrors.NotPaid);
+        }
+
+        // Deposit or withdrawal
+        var result = transaction.Type == TransactionType.Income
+            ? wallet.Deposit(
+                transaction.Amount,
+                transaction.Description,
+                transaction.PaymentDate!.Value,
+                dateTimeService.UtcNow)
+            : wallet.Withdraw(
+                transaction.Amount,
+                transaction.Description,
+                transaction.PaymentDate!.Value,
+                dateTimeService.UtcNow);
+
+        if (!result.IsSuccessful)
+        {
+            return Result.Fail(result.Error);
+        }
+
+        walletRepository.Update(wallet);
+
+        return Result.Ok();
+    }
+}
