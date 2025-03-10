32a776bc5cf341dd7a6d15920a2da11321e7b31b
gerardogarnica <gerardo.garnica@gmail.com>
2025-03-10 12:53:16 -0500

    Add wallet assign to available and to savings command

---
+++ src/Aurora.Coinly.Application/Wallets/AssignToAvailable/AssignToAvailableCommandValidator.cs
@@ -0,0 +1,11 @@
+﻿namespace Aurora.Coinly.Application.Wallets.AssignToAvailable;
+
+internal sealed class AssignToAvailableCommandValidator : AbstractValidator<AssignToAvailableCommand>
+{
+    public AssignToAvailableCommandValidator()
+    {
+        RuleFor(x => x.Amount)
+            .GreaterThan(0)
+            .PrecisionScale(2, 9, true);
+    }
+}
---
+++ src/Aurora.Coinly.Application/Wallets/AssignToSavings/AssignToSavingsCommandValidator.cs
@@ -0,0 +1,11 @@
+﻿namespace Aurora.Coinly.Application.Wallets.AssignToSavings;
+
+internal sealed class AssignToSavingsCommandValidator : AbstractValidator<AssignToSavingsCommand>
+{
+    public AssignToSavingsCommandValidator()
+    {
+        RuleFor(x => x.Amount)
+            .GreaterThan(0)
+            .PrecisionScale(2, 9, true);
+    }
+}
