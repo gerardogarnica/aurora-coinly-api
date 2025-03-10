59bc07733beba05be23646aa738238a83417d707
gerardogarnica <gerardo.garnica@gmail.com>
2025-03-10 08:20:02 -0500

    Add cancellation token to generic repository interface

--- src/Aurora.Coinly.Domain/Abstractions/IRepository.cs
+++ src/Aurora.Coinly.Domain/Abstractions/IRepository.cs
@@ -4,6 +4,6 @@ public interface IRepository<in T> where T : BaseEntity
 {
     IUnitOfWork UnitOfWork { get; }
 
-    Task AddAsync(T entity);
+    Task AddAsync(T entity, CancellationToken cancellationToken);
     void Update(T entity);
 }
