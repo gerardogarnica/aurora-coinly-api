9ba844300079eda61d032e664444a5f28774bba0
gerardogarnica <gerardo.garnica@gmail.com>
2025-03-10 15:19:00 -0500

    Add generic application exception

--- src/Aurora.Coinly.Application/GlobalUsings.cs
+++ src/Aurora.Coinly.Application/GlobalUsings.cs
@@ -1,4 +1,5 @@
-﻿global using Aurora.Coinly.Application.Abstractions.Messaging;
+﻿global using Aurora.Coinly.Application.Abstractions.Exceptions;
+global using Aurora.Coinly.Application.Abstractions.Messaging;
 global using Aurora.Coinly.Application.Abstractions.Time;
 global using Aurora.Coinly.Domain.Abstractions;
 global using Aurora.Coinly.Domain.Shared;
