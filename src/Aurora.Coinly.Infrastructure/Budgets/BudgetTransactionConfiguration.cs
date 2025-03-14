c740c097ebfdfd47ffbfa17c05fc25de6732a61a
gerardogarnica <gerardo.garnica@gmail.com>
2025-03-14 15:34:40 -0500

    Add budget entity type configuration

---
+++ src/Aurora.Coinly.Infrastructure/Budgets/BudgetTransactionConfiguration.cs
@@ -0,0 +1,30 @@
+﻿using Aurora.Coinly.Domain.Budgets;
+using Aurora.Coinly.Domain.Shared;
+
+namespace Aurora.Coinly.Infrastructure.Budgets;
+
+internal sealed class BudgetTransactionConfiguration : IEntityTypeConfiguration<BudgetTransaction>
+{
+    public void Configure(EntityTypeBuilder<BudgetTransaction> builder)
+    {
+        builder.ToTable("budget_transactions");
+
+        builder.HasKey(x => x.Id);
+
+        builder.Property(x => x.Description).HasMaxLength(100);
+
+        builder.OwnsOne(x => x.Amount, y =>
+        {
+            y.Property(x => x.Amount).HasColumnType("numeric(9, 2)");
+            y.Property(x => x.Currency)
+                .HasConversion(x => x.Code, code => Currency.FromCode(code))
+                .HasMaxLength(3);
+        });
+
+        builder
+            .HasOne<Budget>()
+            .WithMany()
+            .HasForeignKey(x => x.BudgetId)
+            .IsRequired();
+    }
+}
