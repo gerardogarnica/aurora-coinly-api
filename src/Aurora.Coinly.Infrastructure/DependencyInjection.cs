37cfd2def986897b4ddfa175c3ecfc3e1d4f47d3
gerardogarnica <gerardo.garnica@gmail.com>
2025-03-14 14:10:18 -0500

    Add infrastructure project configuration

---
+++ src/Aurora.Coinly.Infrastructure/DependencyInjection.cs
@@ -0,0 +1,34 @@
+﻿using Aurora.Coinly.Infrastructure.Database;
+using Aurora.Coinly.Infrastructure.Time;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection.Extensions;
+
+namespace Aurora.Coinly.Infrastructure;
+
+public static class DependencyInjection
+{
+    public static IServiceCollection AddInfrastructureServices(
+        this IServiceCollection services,
+        IConfiguration configuration)
+    {
+        // Database connection
+        var connectionString = configuration.GetConnectionString("AuroraCoinlyConnection");
+
+        services.AddDbContext<ApplicationDbContext>(options =>
+            options
+                .UseNpgsql(
+                    connectionString,
+                    x => x.MigrationsHistoryTable(HistoryRepository.DefaultTableName, ApplicationDbContext.DEFAULT_SCHEMA))
+                .UseSnakeCaseNamingConvention());
+
+        // IUnitOfWork implementation
+        services.AddScoped<IUnitOfWork>(sp => sp.GetRequiredService<ApplicationDbContext>());
+
+        // DateTime services
+        services.TryAddSingleton<IDateTimeService, DateTimeService>();
+
+        return services;
+    }
+}
---
+++ src/Aurora.Coinly.Infrastructure/GlobalUsings.cs
@@ -0,0 +1,4 @@
+﻿global using Aurora.Coinly.Application.Abstractions.Time;
+global using Aurora.Coinly.Domain.Abstractions;
+global using Microsoft.EntityFrameworkCore;
+global using Microsoft.EntityFrameworkCore.Metadata.Builders;
