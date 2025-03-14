80d2de2dbd22249d1b9c22ddb3af3ccc15619d2c
gerardogarnica <gerardo.garnica@gmail.com>
2025-03-14 14:08:55 -0500

    Add database context

---
+++ src/Aurora.Coinly.Infrastructure/Database/ApplicationDbContext.cs
@@ -0,0 +1,7 @@
+﻿namespace Aurora.Coinly.Infrastructure.Database;
+
+public sealed class ApplicationDbContext(
+    DbContextOptions<ApplicationDbContext> options) : DbContext(options), IUnitOfWork
+{
+    internal const string DEFAULT_SCHEMA = "coinly";
+}
