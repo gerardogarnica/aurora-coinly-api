4d4b3997fe1a9885b352a1a2fbef7db9f00416c7
gerardogarnica <gerardo.garnica@gmail.com>
2025-03-21 15:22:59 -0500

    Add base test abstract class

---
+++ src/test/Aurora.Coinly.Domain.UnitTests/GlobalUsings.cs
@@ -0,0 +1,8 @@
+﻿global using Aurora.Coinly.Domain.Abstractions;
+global using Aurora.Coinly.Domain.Budgets;
+global using Aurora.Coinly.Domain.Categories;
+global using Aurora.Coinly.Domain.Methods;
+global using Aurora.Coinly.Domain.Shared;
+global using Aurora.Coinly.Domain.Transactions;
+global using Aurora.Coinly.Domain.UnitTests.Abstractions;
+global using FluentAssertions;
