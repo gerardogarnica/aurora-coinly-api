aa5f30a5c055fe42c9de375d46e5a422fbc5d481
Gerardo Garnica <gerardo.garnica@gmail.com>
2025-04-01 20:26:59 -0500

    Add serializer and migration extensions

---
+++ src/Aurora.Coinly.Api/Extensions/SerializerExtensions.cs
@@ -0,0 +1,18 @@
+﻿using System.Text.Json.Serialization;
+
+namespace Aurora.Coinly.Api.Extensions;
+
+internal static class SerializerExtensions
+{
+    internal static IServiceCollection AddStringEnumConverter(this IServiceCollection services)
+    {
+        services
+            .AddMvc()
+            .AddJsonOptions(options =>
+            {
+                options.JsonSerializerOptions.Converters.Add(new JsonStringEnumConverter());
+            });
+
+        return services;
+    }
+}
