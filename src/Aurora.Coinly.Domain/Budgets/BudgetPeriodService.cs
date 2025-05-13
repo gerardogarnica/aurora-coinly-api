56186428dba2312a3baba1e40b71b52c08b8eb6b
Gerardo Garnica <gerardo.garnica@gmail.com>
2025-05-13 12:15:39 -0500

    Refactor budget repository

--- src/Aurora.Coinly.Domain/Budgets/BudgetPeriodService.cs
+++ src/Aurora.Coinly.Domain/Budgets/BudgetPeriodService.cs
@@ -7,47 +7,49 @@ public class BudgetPeriodService
         return frequency switch
         {
             BudgetFrequency.Weekly => Enumerable.Range(0, 52)
-                                .Select(week =>
-                                {
-                                    var start = new DateOnly(year, 1, 1).AddDays(week * 7);
-                                    var end = new DateOnly(year, 1, 1).AddDays((week + 1) * 7 - 1);
+                .Select(week =>
+                {
+                    var start = new DateOnly(year, 1, 1).AddDays(week * 7);
+                    var end = new DateOnly(year, 1, 1).AddDays((week + 1) * 7 - 1);
 
-                                    return DateRange.Create(start, end);
-                                }),
+                    return DateRange.Create(start, end);
+                }),
 
             BudgetFrequency.Biweekly => Enumerable.Range(0, 26)
-                    .Select(week =>
-                    {
-                        var start = new DateOnly(year, 1, 1).AddDays(week * 14);
-                        var end = new DateOnly(year, 1, 1).AddDays((week + 1) * 14 - 1);
+                .Select(week =>
+                {
+                    var start = new DateOnly(year, 1, 1).AddDays(week * 14);
+                    var end = new DateOnly(year, 1, 1).AddDays((week + 1) * 14 - 1);
 
-                        return DateRange.Create(start, end);
-                    }),
+                    return DateRange.Create(start, end);
+                }),
 
             BudgetFrequency.Monthly => Enumerable.Range(1, 12)
-                    .Select(month =>
-                    {
-                        var start = new DateOnly(year, month, 1);
-                        var end = start.AddMonths(1).AddDays(-1);
+                .Select(month =>
+                {
+                    var start = new DateOnly(year, month, 1);
+                    var end = start.AddMonths(1).AddDays(-1);
 
-                        return DateRange.Create(start, end);
-                    }),
+                    return DateRange.Create(start, end);
+                }),
 
             BudgetFrequency.Quarterly => Enumerable.Range(0, 3)
-                    .Select(quarter =>
-                    {
-                        var start = new DateOnly(year, (quarter * 3) + 1, 1);
-                        var end = start.AddMonths(3).AddDays(-1);
+                .Select(quarter =>
+                {
+                    var start = new DateOnly(year, (quarter * 3) + 1, 1);
+                    var end = start.AddMonths(3).AddDays(-1);
 
-                        return DateRange.Create(start, end);
-                    }),
+                    return DateRange.Create(start, end);
+                }),
 
-            BudgetFrequency.SemiAnnual => [
+            BudgetFrequency.SemiAnnual =>
+                [
                     DateRange.Create(new DateOnly(year, 1, 1), new DateOnly(year, 6, 30)),
                     DateRange.Create(new DateOnly(year, 7, 1), new DateOnly(year, 12, 31))
                 ],
 
-            BudgetFrequency.Annual => [
+            BudgetFrequency.Annual =>
+                [
                     DateRange.Create(new DateOnly(year, 1, 1), new DateOnly(year, 12, 31))
                 ],
 
--- src/Aurora.Coinly.Domain/Budgets/IBudgetRepository.cs
+++ src/Aurora.Coinly.Domain/Budgets/IBudgetRepository.cs
@@ -4,7 +4,5 @@ public interface IBudgetRepository : IRepository<Budget>
 {
     Task<Budget?> GetByIdAsync(Guid id);
     Task<Budget?> GetByCategoryIdAsync(Guid categoryId, int year);
-    Task<IEnumerable<Budget>> GetListAsync(
-        int year,
-        Guid? categoryId);
+    Task<IEnumerable<Budget>> GetListAsync(int year);
 }
