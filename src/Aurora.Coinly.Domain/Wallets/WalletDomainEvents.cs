18b96064d11223b75cf7bf7189b6f569afd19de6
Gerardo Garnica <gerardo.garnica@gmail.com>
2025-07-07 21:12:13 -0500

    Fix WalletSavingsUpdatedEvent parameter names

--- src/Aurora.Coinly.Domain/Wallets/WalletDomainEvents.cs
+++ src/Aurora.Coinly.Domain/Wallets/WalletDomainEvents.cs
@@ -5,10 +5,10 @@ public sealed class WalletBalanceUpdatedEvent(Guid walletId) : DomainEvent
     public Guid WalletId { get; init; } = walletId;
 }
 
-public sealed class WalletSavingsUpdatedEvent(Guid walletId, Money amount, DateOnly AssignedOn, bool IsIncrement) : DomainEvent
+public sealed class WalletSavingsUpdatedEvent(Guid walletId, Money amount, DateOnly assignedOn, bool isIncrement) : DomainEvent
 {
     public Guid WalletId { get; init; } = walletId;
     public Money Amount { get; init; } = amount;
-    public DateOnly AssignedOn { get; init; } = AssignedOn;
-    public bool IsIncrement { get; init; } = IsIncrement;
+    public DateOnly AssignedOn { get; init; } = assignedOn;
+    public bool IsIncrement { get; init; } = isIncrement;
 }
