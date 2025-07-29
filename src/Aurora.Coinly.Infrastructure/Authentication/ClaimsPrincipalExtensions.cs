7a75f5bf9d668a8414d59bc87dc529c31a54d803
Gerardo Garnica <gerardo.garnica@gmail.com>
2025-07-29 10:00:46 -0500

    Add claims principal extensions class

---
+++ src/Aurora.Coinly.Infrastructure/Authentication/ClaimsPrincipalExtensions.cs
@@ -0,0 +1,19 @@
+﻿using Aurora.Coinly.Application.Abstractions.Exceptions;
+using System.Security.Claims;
+
+namespace Aurora.Coinly.Infrastructure.Authentication;
+
+public static class ClaimsPrincipalExtensions
+{
+    public static Guid GetUserId(this ClaimsPrincipal claimsPrincipal)
+    {
+        string? userId = claimsPrincipal?.FindFirst(ClaimTypes.Sid)?.Value;
+
+        if (!Guid.TryParse(userId, out Guid parsedUserId))
+        {
+            throw new AuroraCoinlyException("User identifier is not valid.");
+        }
+
+        return parsedUserId;
+    }
+}
