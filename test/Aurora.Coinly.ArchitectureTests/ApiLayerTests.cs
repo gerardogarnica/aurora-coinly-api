ceeb42a2cd5d392e854be2ca1fc33f2355530114
Gerardo Garnica <gerardo.garnica@gmail.com>
2025-10-08 15:12:08 -0500

    Add architecture api layer tests

---
+++ test/Aurora.Coinly.ArchitectureTests/ApiLayerTests.cs
@@ -0,0 +1,38 @@
+﻿using Aurora.Coinly.Api.Endpoints;
+using NetArchTest.Rules;
+using Shouldly;
+
+namespace Aurora.Coinly.ArchitectureTests;
+
+public class ApiLayerTests : BaseTest
+{
+    [Fact]
+    public void Endpoint_Should_BeSealed()
+    {
+        TestResult testResult = Types.InAssembly(ApiAssembly)
+            .That()
+            .ImplementInterface(typeof(IBaseEndpoint))
+            .And()
+            .AreNotAbstract()
+            .Should()
+            .BeSealed()
+            .GetResult();
+
+        testResult.IsSuccessful.ShouldBeTrue();
+    }
+
+    [Fact]
+    public void Endpoint_Should_BePublic()
+    {
+        TestResult testResult = Types.InAssembly(ApiAssembly)
+            .That()
+            .ImplementInterface(typeof(IBaseEndpoint))
+            .And()
+            .AreNotAbstract()
+            .Should()
+            .BePublic()
+            .GetResult();
+
+        testResult.IsSuccessful.ShouldBeTrue();
+    }
+}
